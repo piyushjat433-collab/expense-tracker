7db6d08f86508c50ab1740d9f4c976dd94ea84f1
Piyush Choudhary <piyushjat433@gmail.com>
2026-05-24 11:37:45 +0530

    Add API handler for transaction categories

---
+++ frontend/api/transactions/categories.js
@@ -0,0 +1,11 @@
+const { CATEGORIES } = require('../categorizer');
+
+module.exports.default = function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  if (req.method === 'OPTIONS') return res.status(200).end();
+
+  const categories = Object.entries(CATEGORIES).map(([name, config]) => ({
+    name, icon: config.icon, color: config.color,
+  }));
+  return res.json({ success: true, data: categories });
+};
